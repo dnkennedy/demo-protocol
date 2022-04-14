f1badb43b800a23753a1ec7c5d7ec468d2e5d583
David Kennedy <David.Kennedy@umassmed.edu>
2022-04-14 12:23:48 -0400

    Config update for dnkennedy settings

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/MulliganF02/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
-  banner: 'This is a demonstration protocol for ReproSchema.',
+  githubSrc: 'https://raw.githubusercontent.com/dnkennedy/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
+  banner: 'This is Dave\'s demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
   backendServer: null
